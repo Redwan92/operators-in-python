6688817a7c41b7ee6ab39d595e5faac533e0672e
Redwan92 <49997435+Redwan92@users.noreply.github.com>
2019-04-26 02:01:13 +0600

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 02:29:02 2019
+
+@author: Anik
+"""
+print("hey hey hey mon kharap tomar ??")
+x=4
+print(x<5 and x<10) 
+print(not(x<5 or x<10))
+y=11
+print(y>5 and y>10) 
+z = ["apple", "banana"]
+k = ["apple", "banana"]
+n = z
+
+print(z is n)
+print(z is not n)
+basket=["apple","banana"]
+print("banana" in basket)
+print("banana" not in  basket)
